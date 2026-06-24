77d29c65d465384c4521218a8bc37dfb95790cbe
Ayush Solanki <aayushdhatri@gmail.com>
2026-06-24 08:30:19 +0530

    Adding exception handler

---
+++ SpringDemoTodo/src/main/java/com/example/demo/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.example.demo.exceptions;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("Resource Not Found! Try Again!");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleAllGeneralExceptions(Exception ex){
+        return ResponseEntity.
+                status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Something went wrong");
+    }
+
+}
